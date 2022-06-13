28f275814ac414a3edcdc1063352c437546f5fd8
Pernas94 <Pernas94@gmail.com>
2022-06-13 22:58:24 +0200

    layout crear jugador completado

--- app/src/main/java/com/example/cenecmayhem/creadorPartidas/CrearPartida.kt
+++ app/src/main/java/com/example/cenecmayhem/creadorPartidas/CrearPartida.kt
@@ -1,5 +1,7 @@
 package com.example.cenecmayhem.creadorPartidas
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -12,13 +14,18 @@ import clases.Partida
 import clases.Usuario
 import com.example.cenecmayhem.MainActivity
 import com.example.cenecmayhem.R
+import com.example.cenecmayhem.Ronda
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class CrearPartida : AppCompatActivity() {
 
     //Bundle
     var user:Usuario?=null
 
-
+    //BBDD
+    val fb: FirebaseFirestore = Firebase.firestore
 
     //Layout y botones
     val txtNombrePartida: TextView by lazy{findViewById(R.id.crPar_nombrePartida)}
@@ -42,25 +49,58 @@ class CrearPartida : AppCompatActivity() {
 
         btnContinuar.setOnClickListener {
 
+            //Compruebo que lo campos no están vacios
             if(txtNombrePartida.text.isNotEmpty()&&txtDescripcionPartida.text.isNotEmpty()){
 
-                //Creamos la partida, momentaneamente sin información de personajes
-                var partida: Partida = Partida(txtNombrePartida.text.toString(), user!!.usuario,checkPublica.isChecked, txtDescripcionPartida.text.toString())
 
-                val intent: Intent =Intent(this@CrearPartida, CrearJugador::class.java)
-                val bundle:Bundle=Bundle()
-                bundle.putSerializable("user", user!!)
-                bundle.putSerializable("partida", partida)
-                intent.putExtras(bundle)
-                this.startActivity(intent)
-            }else{
+                //Compruebo si la partida existe en BBDD
+                fb.collection("partidas").document(txtNombrePartida.text.toString()).get()
+                    .addOnSuccessListener {
+
+                        //Si existe, lo aviso y lo cambio
+                        if(it.exists()) {
+                            Toast.makeText(this@CrearPartida, "Una partida con ese nombre ya existe, debes escoger otro nombre", Toast.LENGTH_LONG).show()
+                            txtNombrePartida.text=""
+                        }else{
+
+                            //Si no existe, aviso antes de ir a la próxima pantalla
+                            val mBuilder= AlertDialog.Builder(this)
+                            mBuilder.setTitle("Aviso")
+                            mBuilder.setMessage("Esta información no podrá ser modificada después de pasar a la siguiente pantalla.\n" +
+                                    "¿Desea continuar?")
+                            mBuilder.setPositiveButton("Confirmar", DialogInterface.OnClickListener{
+                                    dialog, id->
+
+                                //Creamos la partida, momentaneamente sin información de personajes
+                                var partida: Partida = Partida(txtNombrePartida.text.toString(), user!!.usuario,
+                                    checkPublica.isChecked, txtDescripcionPartida.text.toString())
+
+                                val intent: Intent =Intent(this@CrearPartida, CrearJugador::class.java)
+                                val bundle:Bundle=Bundle()
+                                bundle.putSerializable("user", user)
+                                bundle.putSerializable("partida", partida)
+                                intent.putExtras(bundle)
+                                this.startActivity(intent)
+                                this.finish()
+
+                            })
+
+                            mBuilder.setNegativeButton("Cancelar", DialogInterface.OnClickListener{
+                                    dialog, id->
+                                dialog.cancel()
+
+                            })
+
+                            var alert=mBuilder.create()
+                            alert.show()
+                        }
+                    }.addOnFailureListener {
+                        it.printStackTrace()
+                    }
+
+            } else{
                 Toast.makeText(this@CrearPartida, "¡Todos los campos deben estar rellenos!", Toast.LENGTH_SHORT).show()
             }
         }
-
-
-
     }
-
-
 }
